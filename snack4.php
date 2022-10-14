0832394472c8b93947542207c968a986c73f5cef
AlessandroVola <volaalessandro@gmail.com>
2022-10-14 10:42:31 +0200

    snack 4 ok

---
+++ snack4.php
@@ -0,0 +1,13 @@
+<!--Snack 4
+ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
+
+<?php
+    $text = ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates labore, nemo consequatur nihil illo ex quisquam officia ea magnam a maxime corporis, eius ad sequi blanditiis consectetur id recusandae vero.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos provident, temporibus hic accusamus beatae doloribus facere repudiandae voluptatum deserunt quae libero minima consequuntur, quo voluptatem. Aspernatur possimus fugiat eos vero?';
+
+     $array = explode('.', $text);
+
+     var_dump ($array);
+
+?>
